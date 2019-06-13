f29e9eb5db358343b48384fae75c4f791017e2c0
Deroshan Padotan <d.padotan@gmail.com>
2019-06-13 17:59:42 +0200

    Created functions for generating boxplots of methylation data
    One client side and one server side function created

---
+++ R/ds.boxplotMethy.R
@@ -0,0 +1,75 @@
+ds.boxplotMethy <- function(cpg, phenoVar, molecular.data, pheno.data, datasources){
+  
+  #Calling 'histDataByCondition' server side function which returns a list containing a 
+  #vector of methylation values for each level of the phenotypic variable in 'methyl.data'
+  cally <- paste0("returnDataByCondition(cpg=\'", cpg, "\', ", "phenoVar=\'", phenoVar, "\', " ,"molecular.data=", molecular.data, ", ", "pheno.data=", pheno.data,")")
+  datashield.assign(datasources, 'methyl.data', as.symbol(cally))
+  
+  #Retrieving names of phenotypic variable levels
+  phenoLevels = ds.names(x = 'methyl.data')[[1]]
+  
+  #Checking if a server side error was returned
+  if(any(phenoLevels %in% c("phenoLevelsError", "cpgError", "phenoVarError"))){
+    
+    errorMessages=list(paste0("The number of levels associated with the phenotypic variable exceeds what can be plot.\n", 
+                           "Max number of phenotypic levels permitted = 10", sep=''),
+                    "The cpg site specified does not exist", "The phenotypic variable specified does not exist")
+    
+    message(paste(" ", "ERROR(S): ", 
+                  paste0(errorMessages[which(c("phenoLevelsError", "cpgError", "phenoVarError") %in% phenoLevels)], 
+                         collapse = "\n"),
+                  " ", sep="\n"))
+    
+  }else{
+    
+    #Creating a list to store summary data for each phenotypic level
+    plotList = vector("list", length(phenoLevels))
+    
+    counter=1
+    
+    #Looping over levels of phenotypic variables and storing summary data
+    for (i in phenoLevels) {
+      
+      ds.assign(toAssign = paste('methyl.data$', i, sep = ''), newobj = 'phenoLeveliData')
+      ds.asNumeric(x='phenoLeveliData', newobj = 'numPhenoLeveliData')
+      
+      #NB:ds.summary(x) returns an error if length(x) < 5
+      if(ds.length(x='numPhenoLeveliData') >= 5){
+        
+        summaryData = ds.summary(x='numPhenoLeveliData')
+        quantData = summaryData$study1$`quantiles & mean`
+        
+        #Extracting relevant quantile data for boxplot
+        plotList[[counter]] = list(stats = matrix(c(quantData["5%"], quantData["25%"], quantData["50%"], 
+                                                    quantData["75%"], quantData["95%"]) ,5,1), n=10)
+      }
+      
+      counter = counter + 1
+    }
+    
+    
+    #Grouping summary data from each level (only for non null entries in  plotList)
+    groupbxp <- do.call(mapply, c(cbind, Filter(Negate(is.null), plotList)))
+    nonNullLevels = phenoLevels[which(sapply(plotList, Negate(is.null)))]
+    
+    #Generating boxplot
+    bxp(groupbxp, main = paste0("Boxplot of Methylation Values for ", cpg, sep=""), show.names = FALSE)
+    axis(1, at=1:length(nonNullLevels), labels=nonNullLevels )
+    
+    #Generate warning which outputs all phenotypic levels not plotted (<5 observations)
+    if(length(nonNullLevels) != length(phenoLevels)){
+      message(paste(" ", "NOTE: The following levels of the phenotypic variable were excluded",
+                    "      from the boxplot as they had fewer than 5 observations:", " ",
+                    paste0("      ", paste0(phenoLevels[which(sapply(plotList, is.null))], collapse = ", "), sep=""),
+                    " ", sep="\n"))
+    }
+    
+    if(length(phenoLevels) == 1){
+      message(paste(" ", "NOTE: Only 1 level detected for phenotypic variable specified and ",
+                    "      therefore boxplot generated for all samples in data set", " ", sep="\n"))
+      
+    }
+    
+  }
+  
+}
---
+++ R/returnDataByCondition.R
@@ -0,0 +1,68 @@
+returnDataByCondition <- function(cpg, phenoVar, molecular.data, pheno.data){
+  
+  #NB: Assign type function in DataShield
+  
+  errorList = list(phenoLevelsError = FALSE, cpgError = FALSE, phenoVarError = FALSE)
+  
+  #Checking if the phenotypic variable specified exists
+  if((phenoVar %in% colnames(pheno.data)) == FALSE){
+    errorList$phenoVarError = TRUE
+  }else{
+    #Determining the levels of the phenotype variable
+    phenoLevels = levels(as.factor(pheno.data[, phenoVar]))
+    phenoLevelCount = length(phenoLevels)
+    
+    #Checking if the number of phenotypic levels 
+    #exceeds 10 in which case it can't be plotted
+    if(phenoLevelCount > 10){
+      errorList$phenoLevelsError = TRUE
+    }
+    
+  }
+  
+  #Checking if the cpg site specified exists
+  if((cpg %in% molecular.data$feature) == FALSE){
+    errorList$cpgError = TRUE
+  }
+  
+  #If any errors found in previous checks return error list
+  #otherwise process data normally 
+  if(any(sapply(errorList, isTRUE))){
+    
+    errorList = Filter(isTRUE, errorList)
+    return(errorList)
+    
+  }else{
+    
+    #Creating a list to store the methylation data for each level of the phenotype variable
+    methyl.data = vector("list", phenoLevelCount)
+    names(methyl.data) = gsub(" |/|:|'", "_", phenoLevels)
+    
+    #For levels which are integers only add "Level_" prefix to level name in order to 
+    #avoid errors on the client side when reading from data frame and referencing levels
+    names(methyl.data) = lapply(names(methyl.data), function(x) ifelse(!grepl("\\D", x), paste0("Level_", x), x))
+    
+    for (k in 1:phenoLevelCount) {
+      
+      #List of phenotypic data row numbers corresponding to 
+      #level[k] of the phenotypic variable of interest
+      sampleRowNums = which(pheno.data[[phenoVar]]==phenoLevels[k])
+      
+      #Getting the list of sample names which correspond to each level[k] of the phenotypic variable
+      phenoSampleNames = pheno.data[sampleRowNums, 'sample']
+      
+      #Retrieving methlyation values of each sample corresponding to specified 
+      #level of phenotypic variable (only for denoted CpG site)
+      methyl.data[[k]] = t(molecular.data[which(molecular.data$feature==cpg), 
+                                          names(molecular.data) %in% phenoSampleNames])
+      
+    }
+    
+    return(methyl.data)
+    
+  }
+  
+  
+  
+}
+
