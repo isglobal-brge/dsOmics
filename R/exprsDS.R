af4adffe0895823acb562fbf93b6d268646c05b4
JUAN RAMÓN <jrgonzalez@isglobal.lan>
2019-12-12 12:41:05 +0100

    added exprsDS

---
+++ R/exprsDS.R
@@ -0,0 +1,11 @@
+#'
+#' @title Retrieve information on molecular data recorded in eSet and ExpressionSet-derived classes.
+#' @description This function is similar to the Biobase function 'exprs'.
+#' @param x Object, possibly derived from eSet-class.
+#' @return a matrix features as rows, individuals/samples as columns
+#' @author Gonzalez, JR.
+#' @export
+#' 
+exprsDS <- function(x) {
+  Biobase::exprs(x)
+}
