d5823ba8e1b5151340308ba458599981148954a1
JUAN RAMÓN <jrgonzalez@isglobal.lan>
2020-02-06 09:29:47 +0100

    added RSE to limma

--- R/limmaDS.R
+++ R/limmaDS.R
@@ -2,20 +2,28 @@
 #' @title Differential expression analysis using limma
 #' @description To be supplied
 #' @param model
-#' @param eSet
+#' @param Set
 #' @param sva
 #' @return a matrix with genes ordered by p-value
 #' @author Gonzalez, JR.
 #'
 #' @export 
-limmaDS <- function(model, eSet, sva){
+limmaDS <- function(model, Set, sva){
+  
+  if(inherits(Set, "ExpresionSet"))
+    exprsData <- Biobase::pData(x)
+  else if (inherits(x, "RangedSummarizedExperiment"))
+    exprsData <- SummarizedExperiment::colData(x)
+  else
+    stop("Set must be a 'eSet' or 'rse' object")
+  
   design <- stats::model.matrix(stats::as.formula(model), 
-                                data=Biobase::pData(eSet))
+                                data=exprsData)
   if (isTRUE(sva)){
     ds.cbind(c('design', 'sva'), newobj='design')
   }
   
-  fit <- limma::lmFit(eSet, design)
+  fit <- limma::lmFit(Set, design)
   fit <- limma::eBayes(fit)
   ans <- limma::topTable(fit, n=Inf)
   return(ans)
