0aeeb4d2d0b92930d016daa7eb887c5da83776b8
JUAN RAMÓN <jrgonzalez@isglobal.lan>
2020-02-12 15:03:36 +0100

    Update limmaDS.R

--- R/limmaDS.R
+++ R/limmaDS.R
@@ -1,8 +1,9 @@
 #'
 #' @title Differential expression analysis using limma
 #' @description To be supplied
-#' @param model
 #' @param Set
+#' @param variable_names
+#' @param covariable_names
 #' @param sva
 #' @return a matrix with genes ordered by p-value
 #' @author Gonzalez, JR.
@@ -12,7 +13,7 @@
 limmaDS <- function(Set, variable_names, covariable_names, sva){
   res <- MEAL::runPipeline(set = Set, 
                            variable_names = variable_names,
-                           covariable_names = covariable_names
+                           covariable_names = covariable_names,
                            sva=sva)
   ans <- MEAL::getProbeResults(res)
   return(ans)
