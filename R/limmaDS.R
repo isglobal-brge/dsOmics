03f6a7f571a5ff845b4f63fe33e22a80b654cc05
JUAN RAMÓN <jrgonzalez@isglobal.lan>
2020-02-12 17:25:42 +0100

    annot added

--- R/limmaDS.R
+++ R/limmaDS.R
@@ -5,16 +5,17 @@
 #' @param variable_names
 #' @param covariable_names
 #' @param sva
+#' @param annotCols
 #' @return a matrix with genes ordered by p-value
 #' @author Gonzalez, JR.
 #'
 #' @export 
 #' 
-limmaDS <- function(Set, variable_names, covariable_names, sva){
+limmaDS <- function(Set, variable_names, covariable_names, sva, annotCols){
   res <- MEAL::runPipeline(set = Set, 
                            variable_names = variable_names,
                            covariable_names = covariable_names,
                            sva=sva)
-  ans <- MEAL::getProbeResults(res)
+  ans <- MEAL::getProbeResults(res, fNames=annotCols)
   return(ans)
 }
