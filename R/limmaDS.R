9fd68f7ca01e70953dc4f9449d8cf9020239d196
JUAN RAMÓN <jrgonzalez@isglobal.lan>
2020-02-12 13:51:18 +0100

    Update limmaDS.R

--- R/limmaDS.R
+++ R/limmaDS.R
@@ -10,7 +10,7 @@
 #' @export 
 #' 
 limmaDS <- function(model, Set, sva){
-  res <- runDiffMeanAnalysis(set = Set, model = model, sva=sva)
+  res <- MEAL::runDiffMeanAnalysis(set = Set, model = model, sva=sva)
   ans <- getProbeResults(res)
   return(ans)
 }
