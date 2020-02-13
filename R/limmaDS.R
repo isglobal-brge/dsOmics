34117871382b68ad7ec3874041089a35e7a0805c
JUAN RAMÓN <jrgonzalez@isglobal.lan>
2020-02-13 12:04:02 +0100

    ...

--- R/limmaDS.R
+++ R/limmaDS.R
@@ -11,7 +11,26 @@
 #'
 #' @export 
 #' 
-limmaDS <- function(Set, variable_names, covariable_names, sva, fNames=NULL){
+limmaDS <- function(Set, variable_names, covariable_names, type, sva, fNames=NULL){
+  
+  if (type==2){
+    if(inherits(Set, "ExpressionSet")){
+      Set.counts <- Biobase::exprs(Set)
+      pheno <- Biobase::pData(Set)
+    }
+    else if (inherits(Set, "RangedSummarizedExperiment")){
+      Set.counts <- SummarizedExperiment::assay(Set)
+      pheno <- SummarizedExperiment::colData(Set)
+    }
+    ff <- paste("~", paste(c(variable_names, covariable_names), collapse="+")) 
+    design <- model.matrix(formula(ff), data=pheno)
+    v <- limma::voom(Set.counts, design = design)$E
+    if(inherits(Set, "ExpressionSet"))
+      Biobase::exprs(Set) <- v
+    else if (inherits(Set, "RangedSummarizedExperiment"))
+      SummarizedExperiment::assay(Set) <- v
+  }
+    
   res <- MEAL::runPipeline(set = Set, 
                            variable_names = variable_names,
                            covariable_names = covariable_names,
