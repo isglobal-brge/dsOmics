a3f7351287c28d8c1d76da31e7948b7509f38efd
Xavier Escribà Montagut <escri11@yahoo.es>
2020-11-02 15:48:18 +0100

    test1

--- R/alleleFrequencyDS.R
+++ R/alleleFrequencyDS.R
@@ -10,11 +10,12 @@ alleleFrequencyDS <- function(genoData, sexcol, male, female){
   
   
   if(inherits(genoData, "GenotypeData")){
+    return(list(class(genoData@scanAnnot@data), genoData@scanAnnot@data))
     genoData@scanAnnot@sexCol <- sexcol
     genoData@scanAnnot@data[,sexcol][genoData@scanAnnot@data[,sexcol] == male] <- "M"
     genoData@scanAnnot@data[,sexcol][genoData@scanAnnot@data[,sexcol] == female] <- "F"
     
-    return(alleleFrequency(genoData, verbose = FALSE))
+    return(GWASTools::alleleFrequency(genoData, verbose = FALSE))
   }
   else(stop(paste0("Object of incorrect type [", class(genoData), "] alleleFrequency requires object of type GenotypeData")))
   
