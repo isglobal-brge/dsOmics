70f2dbdbdbe35bf42d58edf1a174b10adaf7ac33
Xavier Escribà Montagut <escri11@yahoo.es>
2020-11-03 17:54:07 +0100

    library calls added

--- R/getSNPSbyGenDS.R
+++ R/getSNPSbyGenDS.R
@@ -18,20 +18,20 @@ getSNPSbyGenDS <- function(gds, old_assign, ...){
   
   df <- AnnotationDbi::select(org.Hs.eg.db::org.Hs.eg.db, keys = dots, columns = c("SYMBOL", "ENTREZID"), keytype = "SYMBOL")
   gr <- GenomicFeatures::genes(TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene, filter=list(gene_id=df$ENTREZID))
-  seqlevelsStyle(gr) <- "NCBI"
+  GenomeInfoDb::seqlevelsStyle(gr) <- "NCBI"
   
   snp.ranges <- NULL
   for(i in 1:length(dots)){
     snp.ranges <- c(snp.ranges, seq(gr@ranges[i]@start, length.out = gr@ranges[i]@width))
   }
   
-  snp.ids <- getSnpID(gds)[getPosition(gds) %in% snp.ranges]
+  snp.ids <- GWASTools::getSnpID(gds)[GWASTools::getPosition(gds) %in% snp.ranges]
   
   new_gds <- tempfile(fileext = ".gds")
   GWASTools::close(gds)
   GWASTools::gdsSubset(gds@filename, new_gds, snp.include = snp.ids, allow.fork = TRUE)
   
-  gds_new <- GdsGenotypeReader(new_gds)
+  gds_new <- GWASTools::GdsGenotypeReader(new_gds)
   
   return(gds_new)
   
