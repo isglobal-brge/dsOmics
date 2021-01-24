19788596df33c81d078d1d34f77afe9c56b039d6
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2021-01-24 18:42:57 +0100

    added tablePheno

---
+++ R/tablePhenoDS.R
@@ -0,0 +1,14 @@
+#' @title Retrieve the number of counts at each factor levels for metadata covariates of eSets and RangedSummarizedExperiments.
+#' @description This function is similar to the base function \code{table}.
+#' @param x Object, possibly derived from eSet-class or RangedSummarizedExperiment.
+#' @return a numeric value for each factor level
+#' @author Gonzalez, JR.
+#'
+#' @export
+
+nSamplesDS <- function(x){
+  if(inherits(x, c("ExpressionSet", "SummarizedExperiment","RangedSummarizedExperiment")))
+    return(table(x))
+  else
+    stop("implements the proper method")
+} 
