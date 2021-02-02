1366497e8fb1bde1b90afafc07149e2945166c56
Xavier Escribà Montagut <xavier.escriba@isglobal.org>
2021-02-02 11:20:19 +0100

    .

--- R/featureDataDS.R
+++ R/featureDataDS.R
@@ -1,7 +1,7 @@
 #' @title Retrieve information on features recorded in eSet-derived objects.
 #' 
 #' @description This function is similar to the Biobase function \code{featureData}.
-#' @param object Object, possibly derived from \code{eSet-class}.
+#' @param x Object, possibly derived from \code{eSet-class}.
 #' @return an object containing information on both variable values and variable meta-data.
 #' @author Gonzalez, JR.
 #'
