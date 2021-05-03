db2d752537d7dd65c1894a750c3e01650ec9aa50
Xavier Escribà Montagut <xescriba@isglobal.lan>
2021-05-03 17:09:08 +0200

    added sanity checks

--- R/plotPCASNPSDS.R
+++ R/plotPCASNPSDS.R
@@ -2,12 +2,19 @@
 #'
 #' @param x 
 #' @param group 
+#' @param geno 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-plotPCASNPSDS <- function(x, feno, feno_id, group){
+plotPCASNPSDS <- function(x, geno, group){
+  
+  if(!is.null(geno)){
+    if(!inherits(geno, 'GenotypeData')){
+      stop('Geno object is not of class "GenotypeData"')
+    }
+  }
   
   #############################################################
   #MODULE 1: CAPTURE THE nfilter SETTINGS                     #
@@ -15,6 +22,11 @@ plotPCASNPSDS <- function(x, feno, feno_id, group){
   nfilter.levels <- as.numeric(thr$nfilter.levels)            #
   #############################################################
   
+  feno <- GWASTools::getScanAnnotation(geno)@data
+  if(is.null(feno[[group]])){
+    stop('Grouping variable [', group, '] is not inside of the genotype file. Check the column
+         names of the covariates file used to create the GenotypeData file')
+  }
   factor.levels.present.in.source <- levels(factor(feno[[group]]))
   num.levels<-length(factor.levels.present.in.source)
   max.allowed.levels<-length(feno[[group]])*nfilter.levels
@@ -26,10 +38,10 @@ plotPCASNPSDS <- function(x, feno, feno_id, group){
              max.allowed.levels," levels in this particular study. This variable has ",num.levels)
     return(list(error.message=error.message))
   } else{
-    if(!all(unlist(feno[,feno_id]) %in% GWASTools::getScanID(x))){
+    if(!all(unlist(feno$scanID) %in% GWASTools::getScanID(x))){
       stop('The covariates file does not contain all the individuals on the GDS')
     } else {
-      return(feno[match(GWASTools::getScanID(x), unlist(feno[,feno_id])),][[group]])
+      return(feno[match(GWASTools::getScanID(x), unlist(feno$scanID)),][[group]])
     }
   }
 }
