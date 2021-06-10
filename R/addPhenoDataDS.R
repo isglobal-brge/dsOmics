3bbd48aa81548c0ce41fa9614d03ff6be309b107
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2021-06-10 12:51:30 +0200

    ds.addPhenoData, added option to merge by column of eSet pheno slot

--- R/addPhenoDataDS.R
+++ R/addPhenoDataDS.R
@@ -6,12 +6,15 @@
 #' @param complete_cases \code{bool} If \code{TRUE} only the matching individuals 
 #' between the ExpressionSet and the phenotypes table will be included on the resulting ExpressionSet. If 
 #' \code{FALSE} all the individuals on the input ExpressionSet will be on the output ExpressionSet
+#' @param alternate_eset_id \code{character} Alternate ID of the eSet pheno data, by default the rownames 
+#' of the eSet pheno data act as ID, use this argument if the ID to merge the individuals is on a column of the pheno data. 
+#' Input NULL for the standard behaviour of using the rownames of the pheno data as ID.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-addPhenoDataDS <- function(x, pheno, identifier, complete_cases){
+addPhenoDataDS <- function(x, pheno, identifier, alternate_eset_id, complete_cases){
   
   if(!(any(identifier %in% colnames(pheno)))){
     stop("Identifier [", identifier, "] is not on the phenotypes table")
@@ -23,12 +26,23 @@ addPhenoDataDS <- function(x, pheno, identifier, complete_cases){
   new_variables <- colnames(pheno)[!(identifier == colnames(pheno))]
   old_variables <- colnames(og_pheno)
   
-  og_individuals <- rownames(og_pheno)
+  if(is.null(alternate_eset_id)){
+    og_individuals <- rownames(og_pheno)
+  } else {
+    og_individuals <- og_pheno[,alternate_eset_id]
+  }
+  
   new_individuals <- pheno[,identifier]
   common_individuals <- new_individuals %in% og_individuals
   
   new_pheno <- pheno[common_individuals,]
   og_pheno <- cbind(og_pheno, og_individuals_id = og_individuals)
+  
+  if(is.null(alternate_eset_id)){
+    rownames_new_pheno <- new_pheno$og_individuals_id
+  } else {
+    rownames_new_pheno <- rownames(og_pheno[og_individuals %in% new_individuals[common_individuals],])
+  }
   
   if(complete_cases == TRUE){
     new_pheno <- dplyr::right_join(og_pheno, new_pheno, by = c("og_individuals_id" = identifier))
@@ -39,7 +53,7 @@ addPhenoDataDS <- function(x, pheno, identifier, complete_cases){
     assay_data <- Biobase::exprs(x)
   }
   
-  rownames(new_pheno) <- new_pheno$og_individuals_id
+  rownames(new_pheno) <- rownames_new_pheno
   new_pheno$og_individuals_id <- NULL
   
   if(any(new_variables %in% old_variables)){stop("Variables conflict between ExpressionSet and new PhenoData")}
