56c804c3f52e317937fac7c25528cf83cbb60fd7
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2021-07-12 16:53:52 +0200

    PRS only pass ROI, no PGS ID

--- R/PRSDS.R
+++ R/PRSDS.R
@@ -24,26 +24,21 @@
 #' @export
 #'
 
-PRSDS <- function(resources, pgs_id, snp_threshold, ...){
+PRSDS <- function(resources, snp_threshold, ...){
   
   ROI <- unlist(list(...))
+  ROI <- data.frame(matrix(ROI[1:(length(ROI)-1)], ncol = as.numeric(ROI[length(ROI)])))
+  if(ncol(ROI) == 5){
+    colnames(ROI) <- c("chr_name", "start", "end", "effect_allele", "effect_weight")
+    ROI$start <- as.numeric(ROI$start)
+    ROI$end <- as.numeric(ROI$end)
+    ROI$chr_name <- as.numeric(ROI$chr_name)
+    ROI$effect_weight <- as.numeric(ROI$effect_weight)
+  } else if(ncol(ROI) == 3){
+    colnames(ROI) <- c("rsID", "effect_allele", "effect_weight")
+    ROI$effect_weight <- as.numeric(ROI$effect_weight)
+  } else{stop()}
   
-  if(is.null(ROI)){
-    # Get PGS data to later calculate genetic risk score
-    ROI <- .retrievePGS(pgs_id)
-  } else{
-    ROI <- data.frame(matrix(ROI[1:(length(ROI)-1)], ncol = as.numeric(ROI[length(ROI)])))
-    if(ncol(ROI) == 5){
-      colnames(ROI) <- c("chr_name", "start", "end", "effect_allele", "effect_weight")
-      ROI$start <- as.numeric(ROI$start)
-      ROI$end <- as.numeric(ROI$end)
-      ROI$chr_name <- as.numeric(ROI$chr_name)
-      ROI$effect_weight <- as.numeric(ROI$effect_weight)
-    } else if(ncol(ROI) == 3){
-      colnames(ROI) <- c("rsID", "effect_allele", "effect_weight")
-      ROI$effect_weight <- as.numeric(ROI$effect_weight)
-    } else{stop()}
-  }
   # Get the found SNPs, positions and alleles and merge into data frame
   found_rs <- do.call(c,lapply(resources, function(x){
     GWASTools::getVariable(x, "snp.rs.id")
@@ -147,61 +142,4 @@ PRSDS <- function(resources, pgs_id, snp_threshold, ...){
   p_prs_nw <- SNPassoc::pscore(prs_nw, colnames(geno))
   # TODO devolver todo como una tabla mejor??
   return(data.frame(prs = prs, prs_nw = prs_nw, p_prs_nw = p_prs_nw, n_snps = n_snps))
-}
-
-#' @title Internal function: Get PGS catalog table of polygenic risks
-#'
-#' @param pgs_id \code{character} ID of the PGS catalog to be used to calculate the polygenic risk score. 
-#' Polygenic Score ID & Name from https://www.pgscatalog.org/browse/scores/
-#'
-#' @return \code{data.frame} with the columns: \cr
-#' - If chr_name and poition are found: \cr
-#' + start \cr
-#' + end \cr
-#' + effect_allele \cr
-#' + effect_weight \cr
-#' + weight_type (if present on the catalog) \cr
-#' - If rsID is found: \cr
-#' + rsID \cr
-#' + effect_allele \cr
-#' + effect_weight \cr
-#' + weight_type (if present on the catalog) \cr
-#'
-
-.retrievePGS <- function(pgs_id){
-  pgs <- httr::GET(paste0("https://www.pgscatalog.org/rest/score/", pgs_id))
-  pgs_text <- httr::content(pgs, "text")
-  pgs_scoring_file <- jsonlite::fromJSON(pgs_text, flatten = TRUE)$ftp_scoring_file
-  if(is.null(pgs_scoring_file)){stop('[', pgs_id, '] Not found in www.pgscatalog.org')}
-  
-  # Download scoring file
-  destination <- tempfile(fileext = ".txt.gz")
-  download.file(pgs_scoring_file, destination)
-  
-  # Unzip file
-  unziped_file <- tempfile(fileext = ".txt")
-  R.utils::gunzip(destination, unziped_file)
-  
-  # Read file into R
-  # Logic https://www.pgscatalog.org/downloads/
-  scorings <- read.delim(unziped_file, comment.char = "#")
-  if(c("chr_name", "chr_position") %in% colnames(scorings)){
-    data <- data.frame(chr_name = scorings$chr_name,
-                       start = scorings$chr_position,
-                       end = scorings$chr_position,
-                       # reference_allele = scorings$reference_allele,
-                       effect_allele = scorings$effect_allele,
-                       effect_weight = scorings$effect_weight)
-    if("weight_type" %in% colnames(scorings)){
-      data <- data %>% tibble::add_column(weight_type = scorings$weight_type)}
-    return(data)
-  } else {
-    data <- data.frame(rsID = scorings$rsID,
-                       # reference_allele = scorings$reference_allele,
-                       effect_allele = scorings$effect_allele,
-                       effect_weight = scorings$effect_weight)
-    if("weight_type" %in% colnames(scorings)){
-      data <- data %>% tibble::add_column(weight_type = scorings$weight_type)}
-    return(data)
-  }
 }
