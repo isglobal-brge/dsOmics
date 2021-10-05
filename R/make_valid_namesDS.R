63046f8f2eb7bb2822409662bda43d0176ca5060
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2021-10-05 15:25:14 +0200

    renamed function

---
+++ R/make_valid_namesDS.R
@@ -0,0 +1,18 @@
+#' @title Rename column names and character variables
+#'
+#' @description Passes the column names and the character variables through \code{make.names}, this creates 
+#' names without any special characters so there are no problems for accessing any columns or any category 
+#' when using the DataSHIELD parser.
+#'
+#' @param x \code{data.frame} Entry table to modify
+#'
+#' @return \code{data.frame} with the column names and character columns updated
+#' @export
+#'
+#' @examples
+make_valid_namesDS <- function(x){
+  x <- eval(parse(text=x), envir = parent.frame())
+  colnames(x) <- make.names(colnames(x))
+  x <- x %>% mutate(across(where(is.character), make.names))
+  return(x)
+}
