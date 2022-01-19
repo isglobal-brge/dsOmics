b93722ee32e7938370b665586fd956bee4610523
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2022-01-19 12:00:03 +0100

    Update getVariable.R

--- R/getVariable.R
+++ R/getVariable.R
@@ -9,9 +9,13 @@
 #' @examples
 getVariable <- function(x, slot){
   if(slot %in% c("snp.id", "snp.rs.id", "snp.position", "snp.chromosome", "snp.allele")){
-    results <- do.call(c,lapply(x, function(i){
-      GWASTools::getVariable(i, slot)
-    }))
+    if(inherits(x, "list")){
+      results <- do.call(c,lapply(x, function(i){
+        GWASTools::getVariable(i, slot)
+      }))
+    } else {
+      results <- GWASTools::getVariable(x, slot)
+    }
     return(results)
   } else {stop('[', slot, '] Not valid slot')}
 }
