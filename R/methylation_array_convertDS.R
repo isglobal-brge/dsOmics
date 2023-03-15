78fee19320cdf360db7ec1aed2fb07ee4c533951
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-03-15 10:31:40 +0100

    docs: update

--- R/methylation_array_convertDS.R
+++ R/methylation_array_convertDS.R
@@ -1,11 +1,20 @@
-#' @title Convert methylation arrays
+#' @title Convert a Methylation ExpressionSet from EPIC to 450k Array
 #'
-#' @param eSet `ExpressionSet` to convert
-#' @param objective_array `character` Array to which conver (`"450k"` or `"epic"`)
+#' @description This function subsets a methylation ExpressionSet from an EPIC array (850k probes) to a 450k array or vice versa.
 #'
-#' @return `ExpressionSet` converted
+#' @param eSet A methylation ExpressionSet object containing either EPIC or 450k array data.
+#' @param objective_array A character string specifying the target array type. Can be either "450k" or "epic".
+#'
+#' @return An ExpressionSet object containing the subset of probes based on the target array type.
 #' @export
 #'
+#' @examples
+#' # Load example data as 'eSet'
+#' # Convert the eSet from EPIC array to 450k array
+#' eSet_450k <- methylation_array_convertDS(eSet, objective_array = "450k")
+#' # Convert the eSet from 450k array to EPIC array
+#' eSet_epic <- methylation_array_convertDS(eSet, objective_array = "epic")
+
 methylation_array_convertDS <- function(eSet, objective_array){
 
   if(objective_array == "450k"){
